d1976b9fe0fc544e7db52bdfa24da6caccdd5516
sharonmaswai <chepmaswai@gmail.com>
2019-05-31 12:53:42 +0300

    add configurationin the config file

---
+++ app/main/errors.py
@@ -0,0 +1,11 @@
+class Config:
+    pass
+class ProdConfig(Config):
+    pass
+class DevConfig(Config):
+    DEBUG=True
+
+config_options={
+    'development': DevConfig,
+    'production':ProdConfig
+}
