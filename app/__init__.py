c7b42054c8c2694b1da8a1b7e32e2c9a25444e65
sharonmaswai <chepmaswai@gmail.com>
2019-05-31 15:28:42 +0300

    add background image a heading styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from config import config_options
 from flask import Flask
 from .main import Blueprint
+from flask_bootstrap import Bootsrap 
+
+bootstrap= Bootsrap()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,6 +15,8 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
+    #Initializing extentions.
+    bootstrap.init_app(app)
 
     return app
