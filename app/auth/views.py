000028c9124c675a28e916931574ff683fdb81cd
sharonmaswai <chepmaswai@gmail.com>
2019-06-02 00:50:23 +0300

    add logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@ from flask import render_template,redirect,url_for
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
+from flask_login import login_user,logout_user,login_required
 
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -28,3 +29,9 @@ def login():
 
     title = "Blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
