2ac493a81e947408c6634fdc4bb15449c4ddf0fc
sharonmaswai <chepmaswai@gmail.com>
2019-06-01 21:19:31 +0300

    create an add_post route in views

--- app/main/views.py
+++ app/main/views.py
@@ -8,6 +8,30 @@ def index():
 
     return render_template('index.html', name=name)
 
+@main.route('/new_blog', methods=['GET', 'POST'])
+def new_blog():
+    blogform=BlogForm()
+
+    if blogform.validate_on_submit():
+
+        blog_title=blogform.title.data
+        blog=blogform.blog_data.data
+        url=blogform.photo-url.data
+
+        new_blog=Blog(title=blog-title, blog-content=blog, date_posted=datetime.now(),photo_url=url)
+        new_blog.save_blog()
+
+        send_blogs(new_blog)
+        return redirect(url_for('main.blog',id=new_blog.id))
+
+@main.route('/blog/<int:id>', methods=['GET','POST'])
+def blog(id):
+    post_blog=Blog.query.get(id)
+
+    if get_blog is None:
+        abort(404)
+
+    
 
 
 
