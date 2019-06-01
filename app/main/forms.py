d5bb546aad4170586850b670082f514bd355f9f5
sharonmaswai <chepmaswai@gmail.com>
2019-06-01 22:56:26 +0300

    add comments form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,6 +4,11 @@ from wtforms.validators import Required
 
 class BlogForm(FlaskForm):
     title = StringField('Title', validators=[Required()],render_kw={"placeholder": "Title"})
-    blog_data = TextAreaField('Type Blog here', validators=[Required()])
+    blog_data = TextAreaField('Type a Blog here', validators=[Required()])
     photo_url = StringField('Photo URL', validators=[Required()], render_kw={'placeholder': 'Photo URL'})
-    post = SubmitField('Post Blog')
+    post = SubmitField('Post')
+class CommentForm(FlaskForm):
+    name = StringField('Name',validators=[Required()],render_kw={"placeholder": "Name"})
+    email = StringField('Email', validators=[Required()],render_kw={"placeholder": "Email"})
+    comment_data = TextAreaField('Enter Comment', validators=[Required()],render_kw={"placeholder": "Comment"})
+    post = SubmitField('Post Comment')
