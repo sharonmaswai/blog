9d33742cbceb2884f6df39ff2d11171b2568dba0
sharonmaswai <chepmaswai@gmail.com>
2019-06-01 21:27:45 +0300

    add comments model

--- app/main/views.py
+++ app/main/views.py
@@ -31,8 +31,19 @@ def blog(id):
     if get_blog is None:
         abort(404)
 
+    comment_form = CommentForm()
+
+    if comment_form.validate_on_submit():
+        user_name = comment_form.name.data
+        user_email = comment_form.email.data
+        user_comment = comment_form.comment_data.data
+
+        new_comment = Comment(name=user_name,email=user_email,comment_content=user_comment,date_comment = datetime.now(),blog_id=id)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.blog',id=id))
     
+    get_comments = Comment.get_blog_comments(id)
 
-
-
+    return render_template('blog.html', blog_format=blog_format, get_blog=get_blog, title="Blog", comment_form=comment_form, get_comments=get_comments, comments_count = len(get_blog_comments))
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
+
 
 class Blog(db.Model):
 
@@ -54,3 +56,27 @@ class User(UserMixin,db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)  
+
+class Comment(db.Model):
+
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    comment_content = db.Column(db.String())
+    date_comment = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
+    
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_blog_comments(cls,id):
+        comments = Comment.query.filter_by(blog_id=id).order_by('-id').all()
+        return comments
+    
+    @classmethod
+    def get_single_comment(cls,id_blog,id):
+        comment = Comment.query.filter_by(blog_id=id_blog,id=id).first()
+        return comment
