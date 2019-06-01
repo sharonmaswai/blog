915390eada2a44ec80e455ffb88ab5c6073889ff
sharonmaswai <chepmaswai@gmail.com>
2019-06-01 20:56:17 +0300

    create blog model

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 from app import create_app, db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
