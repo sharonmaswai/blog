c9e2d0367dbb5d3a9ce20520fb2b145ea6946e72
sharonmaswai <chepmaswai@gmail.com>
2019-06-03 17:31:25 +0300

    final commit

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY='lalala'
-export QUOTES_API_KEY="http://quotes.stormconsultancy.co.uk/random.json"
+# export QUOTES_API_KEY="http://quotes.stormconsultancy.co.uk/random.json"
 
 python3.6 manage.py server
